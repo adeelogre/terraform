5dbf9fe31bd4b5bbc3974627ae184ecf09d19f15
Adeel <adeel.ogre@gmail.com>
2021-04-04 17:20:09 +0100

    Added to fetch AMI from Amazon and create EC2 instance with public key

--- main.tf
+++ main.tf
@@ -66,3 +66,36 @@ resource "aws_default_security_group" "myapp-default-sg" {
     Name = "${var.env_prefix}-sg"
   }
 }
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.myapp-default-sg.id]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+}
